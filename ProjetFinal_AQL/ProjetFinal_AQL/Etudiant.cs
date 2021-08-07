c303a5915bae622db26016a5dba444b6decce4c6
faouzi1234 <88059139+faouzi1234@users.noreply.github.com>
2021-08-06 23:58:04 -0400

    etudiant version 1
    creation de la recherche d`un etudiant

--- ProjetFinal_AQL/ProjetFinal_AQL/Etudiant.cs
+++ ProjetFinal_AQL/ProjetFinal_AQL/Etudiant.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
 
 namespace ProjetFinal_AQL
 {
@@ -19,9 +21,9 @@ namespace ProjetFinal_AQL
 
             }
 
-            public Etudiant(int numeroEtudiant, string nom, string prenom)
+            public Etudiant( string nom, string prenom)
             {
-                this.numeroEtudiant = numeroEtudiant;
+                this.numeroEtudiant = this.creationNumero() ;
                 this.nom = nom;
                 this.prenom = prenom;
             }
@@ -55,17 +57,51 @@ namespace ProjetFinal_AQL
             {
                 this.prenom = prenom;
             }
+           private int creationNumero()
+           {
+            Random rand = new Random();
+            return rand.Next(1000000, 9999999);
+           }
 
-            public void affiche()
+        public static void recherche(string numero, TextBox imprime)
             {
-                Console.WriteLine("Numero Etudiant : " + numeroEtudiant + " Nom : " + nom + " Prenom : " + prenom);
+            string path = "../../../base/listeEtudiant/" + numero + ".txt";
+
+            if (File.Exists(path))
+            {
+                imprime.Text = System.IO.File.ReadAllText(path);
+            }
+            else
+            {
+                MessageBox.Show("Etudiant < " + numero + " > introvable","", MessageBoxButtons.OK ,MessageBoxIcon.Error);
+            }
             }
 
-            public void enregistrerEtudiant()
-            {
-                //fichier
-            }
+        public void enregistrerEtudiant(TextBox nomBox, TextBox prenomBox)
+        {
+            string text = "CREATION ETUDIANT\n\n" +
+                      this.getNumeroEtudiant() + " " + this.getPrenom() + " " + this.getNom();
 
+            string student = this.getNumeroEtudiant() + " " + this.getPrenom() + " " + this.getNom();
+            DialogResult resutat = MessageBox.Show(text, "Titre", MessageBoxButtons.YesNo);
+            string verif = resutat.ToString();
+
+            if (verif == "Yes")
+            {
+                nomBox.Text = "";
+                prenomBox.Text = "";
+                Sauve.file("../../../base/listeEtudiant/" + this.getNumeroEtudiant() + ".txt", student);
+                Sauve.file("../../../base/doc/lesEtudiants.txt", student);
+                Sauve.file("../../../base/doc/lesNumeros.txt", this.getNumeroEtudiant().ToString());
+                MessageBox.Show("Etudiant Enregistre");
+            }
+            else
+            {
+                nomBox.Text = this.nom ;
+                prenomBox.Text = this.prenom;
+
+            }
+        }
         }
     }
 
